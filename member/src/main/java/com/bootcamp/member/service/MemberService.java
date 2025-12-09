2a921e5ba1fc630f5bb9c4be4b8c01158c99b055
Fuad Irhandi Wiratama <fuad.wiratama@gdn-commerce.com>
2025-12-09 13:58:42 +0700

    Remove commented recipe code from service classes
    Cleaned up MemberService and ProductService by deleting unused, commented-out recipe-related code to improve code clarity and maintainability.

--- member/src/main/java/com/bootcamp/member/service/MemberService.java
+++ member/src/main/java/com/bootcamp/member/service/MemberService.java
@@ -4,19 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-//  @Autowired
-//  private RecipeRepository recipeRepository;
 
-//  public List<Recipe> getRecipes(Integer size, Integer page) {
-//    Pageable pageable = PageRequest.of(page, size);
-//    return recipeRepository.findAll(pageable).getContent();
-//  }
-//
-//  public Recipe getRecipeByUniqueValue(String uniqueValue) {
-//    return recipeRepository.findByUniqueValue(uniqueValue);
-//  }
-//
-//  public Recipe addRecipe(Recipe recipe) {
-//    return recipeRepository.save(recipe);
-//  }
 }
